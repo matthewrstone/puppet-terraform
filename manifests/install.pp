2aaca3df5e65b649620a25b3195fc12a6e9ee89d
Matthew Stone <matt@souldo.net>
2019-01-13 07:11:54 -0600

    Update install manifest

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,4 +5,14 @@
 # @example
 #   include terraform::install
 class terraform::install {
+  $version = lookup('terraform::version'),
+) {
+  archive { "/tmp/terraform_${version}_linux_amd64.zip" :
+    ensure       => present,
+    extract      => true,
+    extract_path => '/usr/local/bin',
+    source       => "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip",
+    cleanup      => true,
+    creates      => '/usr/local/bin/terraform'
+  }
 }
