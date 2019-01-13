1e22269ded966785c9c9e941ab62c1b14820cff2
Matthew Stone <matt@souldo.net>
2019-01-13 07:34:20 -0600

    relearning relearning ruby

--- lib/facter/terraform.rb
+++ lib/facter/terraform.rb
@@ -2,7 +2,7 @@ Facter.add('terraform_version') do
     setcode do
       check = Facter::Core::Execution.execute('which terraform').chop!
       if check then
-        result = Facter::Core::Execution.execute('terraform version')
+        Facter::Core::Execution.execute('terraform version')
       end
     end
 end
