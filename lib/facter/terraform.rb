5ba1773aa47a21e3189810276313e4f8c0431fd4
Matthew Stone <matt@souldo.net>
2019-01-13 07:49:15 -0600

    You cant chop what isnt there.

--- lib/facter/terraform.rb
+++ lib/facter/terraform.rb
@@ -1,6 +1,6 @@
 Facter.add('terraform_version') do
     setcode do
-      check = Facter::Core::Execution.execute('which terraform').sub!('Terraform v', '').chop!
+      check = Facter::Core::Execution.execute('which terraform').sub!('Terraform v', '')
       if check then
         Facter::Core::Execution.execute('terraform version')
       end
