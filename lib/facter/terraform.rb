6412d42a764ace0ed9451d647431ba220a8f1fc8
Matthew Stone <matt@souldo.net>
2019-01-13 07:44:46 -0600

    Clean up the output for the version fact.

--- lib/facter/terraform.rb
+++ lib/facter/terraform.rb
@@ -1,6 +1,6 @@
 Facter.add('terraform_version') do
     setcode do
-      check = Facter::Core::Execution.execute('which terraform').chop!
+      check = Facter::Core::Execution.execute('which terraform').sub!('Terraform v', '').chop!
       if check then
         Facter::Core::Execution.execute('terraform version')
       end
