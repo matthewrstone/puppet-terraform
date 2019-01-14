124d524757657c05938daa9783d09aad8c5473f9
Matthew Stone <matt@souldo.net>
2019-01-14 05:47:59 -0600

    fix

--- lib/facter/terraform.rb
+++ lib/facter/terraform.rb
@@ -1,6 +1,6 @@
 Facter.add('terraform_version') do
     setcode do
-        check = Facter::Core::Execution.execute('which terrafdorm').chop!
+        check = Facter::Core::Execution.execute('which terrafdorm').strip!
         if !check.nil?
             Facter::Core::Execution.execute('terraform version').sub!('Terraform v', '')
         end
