750fcedf6fef6cc0b026a6ee2fa34e4269de5f0b
Matthew Stone <matt@souldo.net>
2019-01-14 05:33:47 -0600

    Moar ruby

--- lib/facter/terraform.rb
+++ lib/facter/terraform.rb
@@ -1,7 +1,7 @@
 Facter.add('terraform_version') do
     setcode do
       check = Facter::Core::Execution.execute('which terraform').strip!
-      if !check.empty? then
+      unless check.nil?
         Facter::Core::Execution.execute('terraform version').sub!('Terraform v', '')
       end
     end
