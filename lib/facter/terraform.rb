de6de124e4b9131f583e22e77ea725dbe1f92d62
Matthew Stone <matt@souldo.net>
2019-01-18 14:48:30 -0600

    update fact for winders

--- lib/facter/terraform.rb
+++ lib/facter/terraform.rb
@@ -1,6 +1,12 @@
 Facter.add('terraform_version') do
     setcode do
-        if !(Facter::Core::Execution.execute('which terraform').chop!).nil?
+        case Facter.value('kernel')
+        when 'windows'
+            okgo = (Facter::Core::Execution.execute('c:\windows\system32\where.exe terraform')).nil?
+        when 'Linux'
+            okgo = (Facter::Core::Execution.execute('which terraform').chop!).nil?
+        end
+        if okgo == false
             Facter::Core::Execution.execute('terraform version').sub!('Terraform v', '')
         end
     end
